044c60d3a1c77ca5b22149f122c514bd06261fb6
FedeLeva <65309757+FedeLeva@users.noreply.github.com>
2024-01-17 16:30:39 -0300

    Update getCoupon.php

--- backend/getCoupon.php
+++ backend/getCoupon.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "Usuario", "arrozz", "officebd");
+$mysqli = new mysqli("localhost", "root", "", "officebd");
 mysqli_set_charset($mysqli, "utf8");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
